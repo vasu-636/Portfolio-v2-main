e8db2ae923fb21a14f718cc86a07febb2439276f
vasu-636 <vasupatel632006@gmail.com>
2026-04-13 16:14:31 +0530

    Fix Vercel deployment for Yarn build

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css'
 function App() {
   return (
     <>
-      <GlobalStyle></GlobalStyle>
+      <GlobalStyle />
       <Header></Header>
       <Main></Main>
       <Analytics />
